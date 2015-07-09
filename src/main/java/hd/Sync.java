646cbafadf6df48a36f49b1bfc8a0774c353a670
Alex Alexandrov <swined@gmail.com>
2015-07-09 15:57:17 +0300

    m

--- src/main/java/hd/Sync.java
+++ src/main/java/hd/Sync.java
@@ -3,31 +3,31 @@ package hd;
 import com.dropbox.core.*;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Locale;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Sync {
 
     private final Logger log = Logger.getLogger(getClass().getName());
-    private final String login;
-    private final String password;
     private final DbxClient client;
 
-    public Sync(String login, String password, String token) {
-        this.login = login;
-        this.password = password;
+    public Sync(String token) {
         this.client = new DbxClient(new DbxRequestConfig("DbOut/1.0", Locale.getDefault().toString()), token);
     }
 
     @Scheduled(fixedDelay = 15 * 60 * 1000)
     private void sync() {
         try (HDOut hd = new HDOut()) {
-            hd.auth(login, password);
+            Properties conf = loadConfig();
+            hd.auth(conf.getProperty("username"), conf.getProperty("password"));
             Integer unseen = hd.unseen();
             if (unseen != null) {
                 HDOut.Episode ep = hd.episode(unseen);
@@ -41,6 +41,21 @@ public class Sync {
             }
         } catch (Throwable e) {
             log.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    private Properties loadConfig() throws IOException, DbxException {
+        try (InputStream stream = new ByteArrayInputStream(loadFile("/config"))) {
+            return new Properties() {{
+                load(stream);
+            }};
+        }
+    }
+
+    private byte[] loadFile(String path) throws IOException, DbxException {
+        try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
+            client.getFile(path, null, stream);
+            return stream.toByteArray();
         }
     }
 
